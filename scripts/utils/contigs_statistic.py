b9c186f76df4b5129cd2920291b0059bb11c21dc
Shirly-Yang <133876255+Shirly-Yang@users.noreply.github.com>
2026-04-01 20:51:10 +0800

    Update contigs_statistic.py

--- scripts/utils/contigs_statistic.py
+++ scripts/utils/contigs_statistic.py
@@ -1,6 +1,6 @@
 import os
-pathroot='/mnt/nfs/5110v5/wjc/metagenome_BGCs/BIGFAM/GCF_select_fold'
-fileo=open('/mnt/nfs/5110v5/wjc/metagenome_BGCs/BIGFAM/contigs_statistic.txt','w')
+pathroot='/path/to/BIGFAM/GCF_select_fold'
+fileo=open('/path/to/BIGFAM/contigs_statistic.txt','w')
 for i in os.listdir(pathroot):
 	pathbgc=os.path.join(pathroot,i)
 	pathcontig=os.path.join(pathbgc,i+'_high_contig')
